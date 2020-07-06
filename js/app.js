264def29e2e3189da1265752e6396dc108e4710a
Jordan Sinou <jordan.sinou@adventori.com>
2020-07-07 00:54:07 +0200

    feedback tab / mobile

--- js/app.js
+++ js/app.js
@@ -3,14 +3,10 @@ var swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets'
-    },
     loop: true,
-    autoplay: {
-        delay: 2000
-    },
+    // autoplay: {
+    //     delay: 2000
+    // },
     speed: 800
 })
 
