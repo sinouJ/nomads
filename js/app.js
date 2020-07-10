4bf76a81b04c46cf96d9afeedf20f5c8f66ca0df
sinouJ <sinoujordan@gmail.com>
2020-07-10 19:21:53 +0200

    anim js

--- js/app.js
+++ js/app.js
@@ -19,5 +19,40 @@ var slider = new Swiper ('.slider', {
     pagination: {
         type: 'bullets',
         el:'.swiper-pagination'
+    },
+    on: {
+        transitionEnd: function() {
+            var slide = document.querySelector('.swiper-slide-active[data-slide]');
+            var attrSlide = slide.getAttribute('data-slide')
+            var allImgEl = document.querySelectorAll('.iphone img')
+            var imgEl = document.querySelector('[data-id="' + attrSlide + '"]');
+
+            for (let i = 0; i < allImgEl.length; i++) {
+                const ele = allImgEl[i];
+                ele.classList.remove('active')
+            }
+            imgEl.classList.add('active')
+        }
     }
 })
+
+function mockup(el) {
+    var id = el.getAttribute('data-num');
+    var imgEl = document.querySelector('[data-id="'+id+'"]');
+    var allEl = document.querySelectorAll('.articles article')
+    var allImgEl = document.querySelectorAll('.iphone img')
+
+
+    for (var i = 0; i < allEl.length; i++) {
+        var ele = allEl[i];
+        ele.classList.remove('active');
+    }
+
+    for (var i = 0; i < allEl.length; i++) {
+        var ele = allImgEl[i];
+        ele.classList.remove('active');
+    }
+    
+    el.classList.add('active');
+    imgEl.classList.add('active')
+}
